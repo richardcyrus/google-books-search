b9707115a5e4e27d104a5d7811581e0cb46af606
Richard Cyrus <richard.cyrus@rcyrus.com>
2019-02-26 13:42:12 -0500

    Add the saved page and support for delete

---
+++ client/src/pages/saved.js
@@ -0,0 +1,102 @@
+import React from 'react';
+import Container from 'react-bootstrap/Container';
+import Row from 'react-bootstrap/Row';
+import Col from 'react-bootstrap/Col';
+import Card from 'react-bootstrap/Card';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import API from '../utils/API';
+import Header from '../components/Header';
+import BookList from '../components/BookList';
+
+/**
+ * Displays the page for the `/saved` route. List's all saved books
+ * in the database.
+ */
+class SavedPage extends React.Component {
+  constructor(props) {
+    super(props);
+
+    this.loadBooks = this.loadBooks.bind(this);
+    this.handleBookAction = this.handleBookAction.bind(this);
+
+    this.state = {
+      pageTag: 'Your Saved Books of Interest',
+      action: 'delete',
+      books: []
+    };
+  }
+
+  /**
+   * On successful rendering of the page, get all saved books from the
+   * backend.
+   */
+  componentDidMount() {
+    this.loadBooks();
+  }
+
+  /**
+   * Send a call to the backend to get all saved books, and store the
+   * results in state.
+   */
+  loadBooks() {
+    API.getSavedBooks()
+      .then((res) => {
+        const bookList = res.data;
+
+        this.setState({ books: bookList });
+      })
+      .catch((error) => console.log(error));
+  }
+
+  /**
+   * Handle the removal of a book from the backend datastore.
+   *
+   * @param {*} id The mongoDB _id to use for removing a book.
+   */
+  handleBookAction(id) {
+    API.deleteBook(id)
+      .then(() => this.loadBooks())
+      .catch((error) => console.log(error));
+  }
+
+  /**
+   * Render the page elements.
+   */
+  render() {
+    return (
+      <Container>
+        <Row>
+          <Col md={12}>
+            <Header pageTag={this.state.pageTag} />
+          </Col>
+        </Row>
+        <Row>
+          <Col md={12}>
+            <Card className="mt-4 shadow">
+              <Card.Header className="border-bottom-0">
+                <h3>
+                  <strong>
+                    <FontAwesomeIcon icon="download" /> Saved Books
+                  </strong>
+                </h3>
+              </Card.Header>
+              <Card.Body>
+                {!this.state.books.length ? (
+                  <h2 className="text-center">No Saved Books</h2>
+                ) : (
+                  <BookList
+                    books={this.state.books}
+                    handleBookAction={this.handleBookAction}
+                    action={this.state.action}
+                  />
+                )}
+              </Card.Body>
+            </Card>
+          </Col>
+        </Row>
+      </Container>
+    );
+  }
+}
+
+export default SavedPage;
--- client/src/routers/AppRouter.js
+++ client/src/routers/AppRouter.js
@@ -3,6 +3,7 @@ import { BrowserRouter as Router, Route } from 'react-router-dom';
 import AppNav from '../components/AppNav';
 import AppFooter from '../components/AppFooter';
 import SearchPage from '../pages/search';
+import SavedPage from '../pages/saved';
 
 /**
  * Manage the registration of react-router-dom routes for the entire
@@ -16,6 +17,7 @@ function AppRouter() {
       <React.Fragment>
         <AppNav />
         <Route exact path="/" component={SearchPage} />
+        <Route exact path="/saved" component={SavedPage} />
         <AppFooter />
       </React.Fragment>
     </Router>
